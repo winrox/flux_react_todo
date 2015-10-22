962039e4b7d9b5790f55f90f7fbd7405121f5bb9
winrox <winnie.mabee@gmail.com>
2015-10-22 16:52:54 -0500

    form with add button renders

--- js/components/main.js
+++ js/components/main.js
@@ -3,14 +3,21 @@ var Actions = require('../actions/actions.js');
 var Store = require('../stores/store.js');
 
 var MainApp = React.createClass({
+
   handleClick: function() {
     Actions.addItem('something');
   },
 
   render: function() {
     return (
-      <div>
-      TESTING THIS
+      <div className="form-inline">
+        <div className="form-group">
+          <label for="newTodo">NEW TODO:</label>
+            <input type="text" class="form-control" id="newTodo" placeholder="tell 'your mom' joke" />
+          <div className="form-group">
+            <button onClick={this.handleClick} className="submit" type="button">ADD</button>
+          </div>
+        </div>
       </div>
     )
   }
