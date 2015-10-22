24eafa8c543ebfe13c438ea40ee7ba465be8b113
winrox <winnie.mabee@gmail.com>
2015-10-22 18:51:21 -0500

    add button now updates state and adds todo to collection

--- js/components/main.js
+++ js/components/main.js
@@ -4,22 +4,38 @@ var Store = require('../stores/store.js');
 
 var MainApp = React.createClass({
 
-  handleClick: function() {
-    Actions.addItem('something');
+  getInitialState: function() {
+    return {
+      // existing todos OR nothing
+      value: this.props.value || ''
+    };
   },
 
   render: function() {
     return (
       <div className="form-inline">
         <div className="form-group">
-          <label for="newTodo">NEW TODO:</label>
-            <input type="text" class="form-control" id="newTodo" placeholder="tell 'your mom' joke" />
+          <label htmlFor="newTodo">NEW TODO:</label>
+            <input onChange={this.onChange} type="text" className="form-control" id="newTodo" placeholder="tell 'your mom' joke" />
           <div className="form-group">
             <button onClick={this.handleClick} className="submit" type="button">ADD</button>
           </div>
         </div>
       </div>
     )
+  },
+
+  // handle text changes in the add todo input
+  onChange: function(event) {
+    this.setState({
+      value: event.target.value
+    });
+  },
+
+  // create an action based on the current state
+  handleClick: function() {
+    console.log(this.state.value);
+    Actions.addItem(this.state.value);
   }
 });
 
--- js/stores/store.js
+++ js/stores/store.js
@@ -1,18 +1,59 @@
 var Dispatcher = require('../dispatcher/dispatcher.js');
 var EventEmitter = require('events').EventEmitter;
 var Assign = require('object-assign');
+var Store = require('../stores/store.js');
+var Constants = require('../constants/constants.js');
 
 var CHANGE_EVENT = 'change';
 
+var todos = {}; // collection of todo items
+
+// add todo item to todo collection
+function add(todoText) {
+  var id = Date.now(); // use timestamp to create unique id for each newly added todo item
+  todos[id] = {
+    id: id,
+    completeTask: false,
+    todoItem: todoText
+  };
+}
+
 var Store = Assign({}, EventEmitter.prototype, {
+  getAllTodos: function() {
+    return todos;
+  },
+
+  // emit change event to any view listening
   emitChange: function() {
     this.emit(CHANGE_EVENT);
-  }
-});
+  },
 
-Dispatcher.register(function(payload){
-  console.log(payload);
-  return true;
+  // allows views to register their callback function with the store
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  // registers this function with the dispatcher to be called whenever the dispatcher emits an action
+  dispatcherIndex: Dispatcher.register(function(payload) {
+    console.log(payload);
+    var action = payload.action;
+    var todoText;
+
+    // switch statement looks for a matching action case
+    switch(action.actionType) {
+      case Constants.TODO_ADD:
+      todoText = action.text.trim(); //removes white space from beginning and end of todoText
+      if(text !== '') {
+        add(todoText); // puts the todo item into the store
+        Store.emitChange(); // tell the view the store has changed
+        break;
+
+        // add more cases for other action types
+      }
+      return true; //No errors. Needed by promise in dispatcher.
+    }
+  })
+
 });
 
 module.exports = Store;
