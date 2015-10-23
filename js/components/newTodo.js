d430ac9bd9af9b3ef6f630eb10b3c3a72178a0fa
winrox <winnie.mabee@gmail.com>
2015-10-22 19:14:15 -0500

    refactored newTodo into its own component

---
+++ js/components/newTodo.js
@@ -0,0 +1,42 @@
+var React = require('react');
+var Actions = require('../actions/actions.js');
+var Store = require('../stores/store.js');
+
+var NewTodo = React.createClass({
+
+  getInitialState: function() {
+    return {
+      // existing todos OR nothing
+      value: this.props.value || ''
+    };
+  },
+
+  render: function() {
+    return (
+      <div className="form-inline">
+        <div className="form-group">
+          <label htmlFor="newTodo">NEW TODO:</label>
+            <input onChange={this.onChange} type="text" className="form-control" id="newTodo" placeholder="tell 'your mom' joke" />
+          <div className="form-group">
+            <button onClick={this.handleClick} className="submit" type="button">ADD</button>
+          </div>
+        </div>
+      </div>
+    )
+  },
+
+  // handle text changes in the add todo input
+  onChange: function(event) {
+    this.setState({
+      value: event.target.value
+    });
+  },
+
+  // create an action based on the current state
+  handleClick: function() {
+    console.log(this.state.value);
+    Actions.addItem(this.state.value);
+  }
+});
+
+module.exports = NewTodo;
