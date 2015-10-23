8544ccf40973f497eecc646c3490dd187cb6f077
winrox <winnie.mabee@gmail.com>
2015-10-23 16:08:43 -0500

    newTodo input clears text and this.state.value after pressing the add button

--- js/components/newTodo.js
+++ js/components/newTodo.js
@@ -16,7 +16,7 @@ var NewTodo = React.createClass({
       <div className="form-inline">
         <div className="form-group">
           <label htmlFor="newTodo">NEW TODO:</label>
-            <input onChange={this.onChange} type="text" className="form-control" id="newTodo" placeholder="tell 'your mom' joke" />
+            <input onChange={this.onChange} type="text" value={this.state.value} className="form-control" id="newTodo" placeholder="tell 'your mom' joke" />
           <div className="form-group">
             <button onClick={this.handleClick} className="submit" type="button">ADD</button>
           </div>
@@ -36,6 +36,7 @@ var NewTodo = React.createClass({
   handleClick: function() {
     console.log(this.state.value);
     Actions.addItem(this.state.value);
+    this.setState({value: ''});
   }
 });
 
